981b6ca42dd0764f369c67885ab763229b34dd74
Guilherme-Soares-Sequeira <guilhermesoaressequeira@hotmail.com>
2022-08-28 23:26:48 +0100

    Cleaned up some questions

--- exs_1/15.hs
+++ exs_1/15.hs
@@ -4,6 +4,5 @@ mediana x y z
   | ((y <= x) && (y >= z)) || ((y <= z) && (y >= x)) = y
   | otherwise = z
 
---mediana' :: Integer a => a -> a -> a -> a ????
 mediana' :: (Num a, Ord a) => a -> a -> a -> a
 mediana' x y z = x + y + z - max (max x y) z - min (min x y) z
